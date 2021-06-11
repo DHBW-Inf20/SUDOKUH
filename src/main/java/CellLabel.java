55ba8ab61b326e04306b44bb0dd0b42336e6ee9d
KremlingP <Philipp.Kremling.pk@gmail.com>
2021-06-11 13:24:16 +0200

    Added first try of a GUI

---
+++ src/main/java/CellLabel.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class CellLabel extends JLabel {
+    private boolean isPredefined = false;
+    private int row;
+    private int col;
+
+    public CellLabel(String text, int row, int col) {
+        super(text);
+        this.row = row;
+        this.col = col;
+    }
+
+    public boolean isPredefined() {
+        return isPredefined;
+    }
+
+    public void setPredefined(boolean isPredefined) {
+        this.isPredefined = isPredefined;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        GUI gui =  new GUI(9);
+        gui.setSize(800,600);
+        gui.setVisible(true);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
