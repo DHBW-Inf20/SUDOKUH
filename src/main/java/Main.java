f4d0ad78ebd3e1251521fe283f2f12305c1f66b3
KremlingP <Philipp.Kremling.pk@gmail.com>
2021-06-12 14:14:23 +0200

    Removed comments from older versions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.LineBorder;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
 public class Main {
 
